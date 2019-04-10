4b070a345d31de3b480a6475f31b93d4e6a543e9
Sergienko Roman <android.garett@gmail.com>
2019-04-10 11:29:21 +0700

    fixed clearInterval() bugs

--- src/js_components/pomidorka.component.js
+++ src/js_components/pomidorka.component.js
@@ -13,11 +13,13 @@ export class PomidorkaTimerComponent extends Component {
     init() {
         this.id_timer = null
 
-        // const form = document.forms.config
-        // const fulltime = form.elements.fulltime
+        this.$configForm = document.forms.config
+        this.pomidorkaTime = '00:03'
+        this.pomidorkaBreak = '5:00'
+        this.pomidorkaLongBreak = '15:00'
 
         this.$time = this.$el.querySelector('.time')
-        this.$time.innerHTML = '15:00'//`${fulltime.value}:00`
+        this.$time.innerHTML = this.pomidorkaTime
 
         this.$startTimer = this.$el.querySelector('.btn-start')
         this.$pauseTimer = this.$el.querySelector('.btn-pause')
@@ -36,22 +38,53 @@ export class PomidorkaTimerComponent extends Component {
         this.$startTimer.addEventListener('click', clickStartHandler.bind(this))
         this.$pauseTimer.addEventListener('click', clickPauseHandler.bind(this))
         this.$stopTimer.addEventListener('click', clickStopHandler.bind(this))
+
+        this.$configForm.addEventListener('change', changeConfigFormInput.bind(this))
     }
 }
 
 // private
+function changeConfigFormInput(event) {
+
+    if (event.target.value.length > 2 || event.target.value > 60) {
+        alert('Не больше 60 минут')
+        event.target.value = '25'
+    }
+    if (event.target.value != 0) {
+        if (!parseInt(event.target.value)) {
+            alert('Это что за дичь тут мне подсунули!?')
+            event.target.value = '25'
+        }
+    }
+    if (event.target.value == 0) {
+        event.target.value = '25'
+    }
+    if (event.target.value < 0) {
+        alert('Это как так???')
+        event.target.value = '25'
+    }
+
+    this.pomidorkaTime = this.$configForm.elements.fulltime.value
+    this.pomidorkaBreak = this.$configForm.elements.breaktime.value
+    this.pomidorkaLongBreak = this.$configForm.elements.longbreaktime.value
+
+    console.log(this.pomidorkaTime, this.pomidorkaBreak, this.pomidorkaLongBreak)
+
+    this.$time.textContent = `${this.pomidorkaTime}:00`
+}
+
 function clickStartHandler(event) {
     visibilityHandler(event.target, this.$pauseTimer)
-    this.$id_timer = setInterval(tickPomidorka.bind(this), 1000)
+    this.id_timer = setInterval(tickPomidorka.bind(this), 1000)
     // fulltime.setAttribute("disabled", "disabled")
 }
 function clickPauseHandler(event) {
     visibilityHandler(event.target, this.$startTimer)
-    clearInterval(this.$id_timer)
+    clearInterval(this.id_timer)
 }
 function clickStopHandler() {
     visibilityHandler(this.$pauseTimer, this.$startTimer)
-    clearInterval(this.$id_timer)
+    clearInterval(this.id_timer)
     addValueToElement(`25:00`, this.$time)
     // $fulltime.removeAttribute("disabled")
 }
@@ -65,11 +98,10 @@ function tickPomidorka() {
 
     if (sec == 0) {
         if (min == 0) {
-            addValueToElement(`25:00`, time)
+            addValueToElement(`${this.pomidorkaTime}`, this.$time)
             visibilityHandler(this.$pauseTimer, this.$startTimer)
 
             // fulltime.removeAttribute("disabled")
-
             clearInterval(this.id_timer)
             return
         }
@@ -95,27 +127,3 @@ function visibilityHandler(toHideElement, toShowElement) {
 function addValueToElement(value, el) {
     el.innerHTML = value
 }
-
-
-// form.elements.fulltime.addEventListener('change', (event) => {
-
-//     if (event.target.value.length > 2 || event.target.value > 60) {
-//         alert('Не больше 60 минут')
-//         event.target.value = '10'
-//     }
-//     if (event.target.value != 0) {
-//         if (!parseInt(event.target.value)) {
-//             alert('Это что за дичь тут мне подсунули!?')
-//             event.target.value = '10'
-//         }
-//     }
-//     if (event.target.value == 0) {
-//         event.target.value = '00'
-//     }
-//     if (event.target.value < 0) {
-//         alert('Это как так???')
-//         event.target.value = '10'
-//     }
-
-//     time.textContent = `${event.target.value}:00`
-// })
